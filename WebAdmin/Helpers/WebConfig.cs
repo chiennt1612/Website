c849e1e92baf0506cb01e556c7c57afa2ca54a8c
Nguyen Thanh Chien <chiennt1612@hotmail.com>
2023-02-13 20:44:05 +0700

    czczx

---
+++ WebAdmin/Helpers/WebConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAdmin.Helpers
+{
+    public class WebConfig
+    {
+        public string CookieName { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string ResponseType { get; set; }
+        public System.Collections.Generic.IList<string> Scope { get; set; }
+        public string WebName { get; set; } = "BacNgocTuan";
+    }
+}
